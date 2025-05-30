02e80ce3cbcbf3d94dad769e02d0f1704172da52
sumit rai <RAIS74184@gmail.com>
2025-05-30 18:29:55 +0530

    ds-open-mistral

---
+++ src/app/config.py
@@ -0,0 +1,3 @@
+# dsenv\Scripts\activate
+#(dsenv) C:\Users\Sumit Amit\OneDrive\Desktop\DataScience-Services\src\app>
+# python __init__.py
---
+++ src/app/service/llmService.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv,dotenv_values
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+from pydantic import BaseModel, Field
+# from langchain_core.pydantic_v1 import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_mistralai import ChatMistralAI
+from service.Expense import Expense
+
+from langchain_core.utils.function_calling import convert_to_openai_tool
+
+class LLMService:
+    def __init__(self):
+        load_dotenv()
+        self.prompt=ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    "you are an expert extraction algorithm"
+                    "Only extract relevant information from the text"
+                    "If you do not know the value of an attribute asked to extract."
+                    "return null for the attribute's value."
+                ),
+                ("human","{text}")
+            ]
+        )
+        self.apiKey=os.getenv('OPENAI_API_KEY')
+        self.llm=ChatMistralAI(api_key=self.apiKey,model="mistral-large-latest")
+        self.runnable=self.prompt | self.llm.with_structured_output(schema=Expense)
+
+    def runLLM(self,message):
+        return self.runnable.invoke({"text":message}) 
